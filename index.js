f334727e52c97d59fb91fe9a5a031c61da41ca24
KINGBOTs1 <ds767365@gmail.com>
2025-08-22 01:20:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Middleware sederhana untuk CORS
-app.use((req, res, next) => {
-    // Mengizinkan akses dari semua origin. Untuk produksi, ganti '*' dengan domain frontend Anda.
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-});
+
 
 
 // === ROUTES / ENDPOINTS DENGAN PRISMA ===
